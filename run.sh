75934492571c24d57d784404037329cbaf8a8ff9
DmitriiDenisov <dmitryhse@gmail.com>
2020-08-19 01:51:52 +0400

    Add port variation

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # docker pull image
 sudo docker pull dmitrydenisov/curr-serv
 PORT=5002
-sudo docker run -p $PORT:$PORT -v $(pwd)/token.txt:/app/token.txt --restart always --name curr_cont -e port=$PORT curr-serv
+sudo docker run -p $PORT:$PORT -v $(pwd)/token.txt:/app/token.txt --restart always --name curr_cont -e port=$PORT -d curr-serv
