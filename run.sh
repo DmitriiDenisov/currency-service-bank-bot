8f837bfec50dd96152f386991b104a6eaeb5adda
DmitriiDenisov <dmitryhse@gmail.com>
2020-08-19 22:35:46 +0400

    Hot fix

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ sudo docker rmi dmitrydenisov/curr-serv
 PORT=5002
 
 # Docker run container
-sudo docker run -p $PORT:$PORT -v $(pwd)/token.txt:/app/token.txt --restart always --name curr_cont -e port=$PORT -d dmitrydenisov/curr-serv
+sudo docker run -p $PORT:$PORT -v $(pwd)/token.txt:/app/token.txt --restart always --name curr_cont -e port=$PORT -d curr-serv
