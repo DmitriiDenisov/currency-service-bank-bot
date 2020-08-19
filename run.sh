f24c136979ff5be05a951da72c08b4288a40f489
DmitriiDenisov <dmitryhse@gmail.com>
2020-08-19 22:29:43 +0400

    Add port variation

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
-# docker pull image
-# docker build -t test_image .
-
 # Pull image
 sudo docker pull dmitrydenisov/curr-serv
+
+# If pull does not work (for example, repo was deleted) then:
+# docker build -t curr-serv .
+
+# Rename iamge
+sudo docker image tag dmitrydenisov/curr-serv:latest curr-serv:latest
+sudo docker rmi dmitrydenisov/curr-serv
+
+# Define port
 PORT=5002
+
 # Docker run container
 sudo docker run -p $PORT:$PORT -v $(pwd)/token.txt:/app/token.txt --restart always --name curr_cont -e port=$PORT -d dmitrydenisov/curr-serv
